1c3793bc047ed7bdeb2fdd0ab2411c874a376af1
alvaroturi11 <20115695@mail.wit.ie>
2025-09-23 12:50:21 +0100

    Delete a task

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -10,6 +10,7 @@ const Task = (props) => {
             <p>{props.children}</p>
             <p className="priority">{props.priority}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
     )
 
